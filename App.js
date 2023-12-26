3f20a9344b94206c1883790174dbeadd0f487121
Marcin Świderek <marcin.swiderek2@gmail.com>
2023-12-26 17:15:25 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 
-export default function App() {
+const TicTacToe = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const handleSquareClick = (index) => {
+    if (board[index] || calculateWinner(board)) {
+      return;
+    }
+
+    const newBoard = board.slice();
+    newBoard[index] = isXNext ? "X" : "O";
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setIsXNext(true);
+  };
+
+  useEffect(() => {
+    const winner = calculateWinner(board);
+    if (winner) {
+      Alert.alert(`Player ${winner} wins!`, "Start a new game?", [
+        { text: "OK", onPress: resetGame },
+      ]);
+    } else if (board.every((square) => square !== null)) {
+      Alert.alert("It's a draw!", "Start a new game?", [
+        { text: "OK", onPress: resetGame },
+      ]);
+    }
+  }, [board]);
+
+  const renderSquare = (index) => (
+    <TouchableOpacity
+      style={styles.square}
+      onPress={() => handleSquareClick(index)}
+    >
+      <Text style={styles.squareText}>{board[index]}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tic Tac Toe</Text>
+      <View style={styles.board}>
+        <View style={styles.row}>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </View>
+        <View style={styles.row}>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </View>
+        <View style={styles.row}>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </View>
+      </View>
+      <TouchableOpacity style={styles.resetButton} onPress={resetGame}>
+        <Text style={styles.resetButtonText}>Reset Game</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  board: {
+    marginBottom: 20,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  square: {
+    width: 60,
+    height: 60,
+    borderWidth: 1,
+    borderColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  squareText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  resetButton: {
+    backgroundColor: "#007BFF",
+    padding: 10,
+    borderRadius: 5,
+  },
+  resetButtonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
+
+export default TicTacToe;
